d78b24fee7a6d529f750555325e843b8440d1a23
Lee Kuan Xin <Lee.Kuan-Xin@heidelberg-mobil.com>
2017-09-18 17:50:16 +0200

    First commit

--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Counter outlets
+    @IBOutlet weak var counterView: CounterView!
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        counterLabel.text = String(counterView.counter)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pushButtonPressed(_ button: PushButton) {
+        if button.isAddButton {
+            counterView.counter += 1
+        } else {
+            if counterView.counter > 0 {
+                counterView.counter -= 1
+            }
+        }
+        counterLabel.text = String(counterView.counter)
+    }
+    
+    
 
 }
 
